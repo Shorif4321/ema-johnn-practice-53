04bda3f534b423831648b97f27e5cbddad6d227d
Shorif4321 <shorifali4321@gmail.com>
2021-09-30 23:12:13 +0600

    ai commett

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h4>THis is Inventory</h4>
+        </div>
+    );
+};
+
+export default Inventory;
